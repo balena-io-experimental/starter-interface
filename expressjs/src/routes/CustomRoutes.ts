1ab405b94d8a1810e3f51abf66aab63aab5a5988
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-02-17 20:49:21 +0000

    Add debug logging to cache middleware

--- expressjs/src/routes/CustomRoutes.ts
+++ expressjs/src/routes/CustomRoutes.ts
@@ -3,19 +3,22 @@ import Logger from '../common/logger'
 
 const router = express.Router()
 
-// Define your custom get route
-router.get('/exampleroute_get', function (_req, res) {
-  res.send('Example route')
+// Define your custom GET route path and actions
+router.get('/example_route_get', function (_req, res) {
+  // Log to the console that this is running
+  Logger.warn('Running content of example_route_get route.')
+  // Actions to take when this route is visited go here.
+  res.send('I am being sent back to the browser')
 })
 
-// Define your custom post route
-router.post('/exampleroute_post', function (req, res) {
-  // Store the value of 'path' submitted as JSON to this endpoint as 'pathVariable'
+// Define your custom POST route
+router.post('/example_route_post', function (req, res) {
+  // Store the value of paramater `path` as `pathVariable`
   const pathVariable = req.body.path
   // Log the message to the console
   Logger.info(pathVariable)
   // Return a message to the caller
-  res.send('done')
+  res.json({ message: 'All done.' })
 })
 
 export default router
