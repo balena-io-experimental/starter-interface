79a46b6ac69055df568e54ecfa2483433c0fca48
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-03-05 14:09:11 +0000

    Add JournalD log download option

--- ui/src/pages/SystemInfo.vue
+++ ui/src/pages/SystemInfo.vue
@@ -1,16 +1,21 @@
 <template>
   <q-page class="q-pa-lg">
-    <system-info />
+    <div class="row">
+      <q-space />
+      <journal-d-logs />
+    </div>
+    <div><system-info /></div>
   </q-page>
 </template>
 
 <script lang="ts">
+import JournalDLogs from 'components/system/JournalDLogs.vue'
 import SystemInfo from 'components/tools/SystemInfo.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'IntSystemInfo',
-  components: { SystemInfo },
+  components: { JournalDLogs, SystemInfo },
   setup() {
     return {}
   }
