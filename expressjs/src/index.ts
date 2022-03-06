f11331e83d49c17eb6ab464a01e46b57cd00bc3e
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-03-06 18:28:43 +0000

    Add Captive Portal

--- expressjs/src/index.ts
+++ expressjs/src/index.ts
@@ -2,6 +2,7 @@ import cors from 'cors'
 import express from 'express'
 import slowDown from 'express-slow-down'
 import BalenaSDKRoutes from '@/routes/v1/BalenaSDKRoutes'
+import CaptivePortal from '@/routes/v1/CaptivePortal'
 import CustomRoutes from '@/routes/v1/CustomRoutes'
 import FileManagerRoutes from '@/routes/v1/FileManagerRoutes'
 import SupervisorRoutes from '@/routes/v1/SupervisorRoutes'
@@ -31,6 +32,7 @@ app.use(express.static('public', { dotfiles: 'allow' }))
 
 // Routes
 app.use(BalenaSDKRoutes)
+app.use(CaptivePortal)
 app.use(CustomRoutes)
 app.use(FileManagerRoutes)
 app.use(SupervisorRoutes)
---
+++ ui/src/components/layouts/CaptivePortal.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="q-pa-lg">
+    <div class="text-center q-mb-md">
+      <q-img :src="qHeaderStyle.logo" style="max-width: 175px" />
+    </div>
+    <div class="text-h4 text-gray-600 text-center mt-2">
+      {{ $t('titles.title') }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { qHeaderStyle } from 'components/styles/qStyles'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IntCaptivePortal',
+  setup() {
+    return {
+      qHeaderStyle
+    }
+  }
+})
+</script>
