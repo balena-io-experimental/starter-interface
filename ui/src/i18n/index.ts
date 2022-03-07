069169b2ffb8c3fa3af256da3c95e17d39e43625
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-03-07 07:44:29 +0000

    Add new languages to switcher

--- ui/src/i18n/index.ts
+++ ui/src/i18n/index.ts
@@ -1,9 +1,15 @@
 import de from './de/index.json'
 import enUS from './en-US/index.json'
+import fr from './fr/index.json'
+import it from './it/index.json'
+import nbNO from './nb-NO/index.json'
 
 export default {
   de: de,
-  'en-US': enUS
+  'en-US': enUS,
+  fr: fr,
+  it: it,
+  'nb-NO': nbNO
 }
 
 // Don't forget to import the Quasar language pack too. Edit the line
