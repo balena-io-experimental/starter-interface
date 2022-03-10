07e241b876932b291cebe576d5715ce6bb5bd072
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-03-10 18:15:09 +0000

    Add forget all networks button

--- ui/src/pages/Configuration.vue
+++ ui/src/pages/Configuration.vue
@@ -24,21 +24,37 @@
           <div><update-device /></div>
         </q-item-section>
       </q-item>
+      <q-item>
+        <q-item-section avatar>
+          <q-icon name="wifi_tethering_off" />
+        </q-item-section>
+
+        <q-item-section>
+          <div><forget-all-wifi /></div>
+        </q-item-section>
+      </q-item>
     </q-list>
     <div class="q-mt-lg"><env-config /></div>
   </q-page>
 </template>
 
 <script lang="ts">
-import EnvConfig from 'components/system/EnvConfig.vue'
 import ChangeHostname from 'components/network/ChangeHostname.vue'
+import EnvConfig from 'components/system/EnvConfig.vue'
+import ForgetAllWifi from 'components/network/ForgetAllWifi.vue'
 import PingSupervisor from 'components/system/PingSupervisor.vue'
 import UpdateDevice from 'components/system/UpdateDevice.vue'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
   name: 'IntConfiguration',
-  components: { ChangeHostname, PingSupervisor, EnvConfig, UpdateDevice },
+  components: {
+    ChangeHostname,
+    EnvConfig,
+    ForgetAllWifi,
+    PingSupervisor,
+    UpdateDevice
+  },
   setup() {
     return {}
   }
