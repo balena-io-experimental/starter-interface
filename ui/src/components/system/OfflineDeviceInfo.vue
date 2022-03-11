6d69332b86195102cda115b4c5c935a925e9690e
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-03-11 09:47:11 +0000

    Reorientate offline page content

--- ui/src/components/system/OfflineDeviceInfo.vue
+++ ui/src/components/system/OfflineDeviceInfo.vue
@@ -1,23 +1,55 @@
 <!-- eslint-disable @intlify/vue-i18n/no-raw-text */ -->
 <template>
   <div v-if="m && f && device && !loading">
-    <h4 class="row items-end q-mt-none q-mb-lg">
-      <q-chip
-        :color="internetConnectivity.status ? 'green' : 'red'"
-        text-color="white"
-      >
-        {{
-          internetConnectivity.status ? $t('wifi.online') : $t('wifi.offline')
-        }}
-      </q-chip>
-    </h4>
+    <q-chip
+      :color="internetConnectivity.status ? 'green' : 'red'"
+      text-color="white"
+    >
+      {{ internetConnectivity.status ? $t('wifi.online') : $t('wifi.offline') }}
+    </q-chip>
     <div><cpu-stats /></div>
-    <div class="row items-start">
-      <q-card flat bordered class="">
-        <q-card-section horizontal>
-          <!-- info -->
+    <div>
+      <div class="q-mb-md">
+        <q-card flat bordered>
+          <q-card-section horizontal>
+            <q-card-section class="full-width">
+              <b class="q-mr-xs">{{ $t('deviceInfo.memory') }}</b>
+              <span> - </span>
+              <span
+                >{{
+                  ((m.data.total - m.data.available) / 1000000).toFixed(0)
+                }}MB / {{ (m.data.total / 1000000).toFixed(0) }}MB</span
+              >
+              <q-linear-progress
+                color="secondary"
+                rounded
+                :value="(m.data.total - m.data.available) / m.data.total"
+                class="q-mt-md"
+              />
+            </q-card-section>
+            <q-card-section class="full-width">
+              <b class="q-mr-xs">{{ $t('deviceInfo.storage') }}</b>
+              <span>({{ f.data[0].mount }})</span>
+              <span> - </span>
+              <span
+                >{{ (f.data[0].used / 1000000000).toFixed(2) }}GB /
+                {{ (f.data[0].size / 1000000000).toFixed(2) }}GB</span
+              >
+              <q-linear-progress
+                color="secondary"
+                rounded
+                :value="f.data[0].used / f.data[0].size"
+                class="q-mt-md"
+              />
+            </q-card-section>
+          </q-card-section>
+        </q-card>
+      </div>
+
+      <div>
+        <q-card flat>
           <q-card-section>
-            <q-list>
+            <q-list class="row">
               <q-item class="q-mb-sm">
                 <q-item-section>
                   <q-item-label caption>{{
@@ -56,44 +88,8 @@
               </q-item>
             </q-list>
           </q-card-section>
-          <!-- end info -->
-          <q-separator vertical />
-          <!-- bars -->
-          <q-card-section>
-            <q-card-section>
-              <b class="q-mr-xs">{{ $t('deviceInfo.memory') }}</b>
-              <span> - </span>
-              <span
-                >{{
-                  ((m.data.total - m.data.available) / 1000000).toFixed(0)
-                }}MB / {{ (m.data.total / 1000000).toFixed(0) }}MB</span
-              >
-              <q-linear-progress
-                color="secondary"
-                rounded
-                :value="(m.data.total - m.data.available) / m.data.total"
-                class="q-mt-md"
-              />
-            </q-card-section>
-            <q-card-section>
-              <b class="q-mr-xs">{{ $t('deviceInfo.storage') }}</b>
-              <span>({{ f.data[0].mount }})</span>
-              <span> - </span>
-              <span
-                >{{ (f.data[0].used / 1000000000).toFixed(2) }}GB /
-                {{ (f.data[0].size / 1000000000).toFixed(2) }}GB</span
-              >
-              <q-linear-progress
-                color="secondary"
-                rounded
-                :value="f.data[0].used / f.data[0].size"
-                class="q-mt-md"
-              />
-            </q-card-section>
-          </q-card-section>
-          <!-- end bars -->
-        </q-card-section>
-      </q-card>
+        </q-card>
+      </div>
     </div>
   </div>
 </template>
