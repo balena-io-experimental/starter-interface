8d897f387cdecdd201393a952c17dd9e91ab5763
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-03-20 13:01:51 +0000

    Show messages on changes to network status

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,8 +2,11 @@
   <div v-if="currentPage == '#/captiveportal'"><captive-portal /></div>
   <router-view v-else />
 </template>
+
 <script lang="ts">
+import { useQuasar } from 'quasar'
 import { defineAsyncComponent, defineComponent } from 'vue'
+import { useI18n } from 'vue-i18n'
 
 export default defineComponent({
   name: 'App',
@@ -13,7 +16,47 @@ export default defineComponent({
     )
   },
   setup() {
+    const $q = useQuasar()
+    // eslint-disable-next-line @typescript-eslint/unbound-method
+    const { t } = useI18n()
+
     const currentPage = location.hash
+
+    // Set a dismiss function var and remove immediately
+    let dismiss = $q.notify({})
+    dismiss()
+
+    // When network is available again
+    window.addEventListener('online', () => {
+      dismiss()
+      dismiss = notify('positive', t('network.network_connection_restored'))
+    })
+
+    // When network is down
+    window.addEventListener('offline', () => {
+      dismiss()
+      dismiss = notify('warning', t('network.network_connection_down'))
+    })
+
+    function notify(type: string, message: string) {
+      const dismiss = $q.notify({
+        type: type,
+        multiLine: true,
+        timeout: 0,
+        actions: [
+          {
+            label: t('general.close'),
+            color: 'white',
+            handler: () => {
+              /* ... */
+            }
+          }
+        ],
+        message: message
+      })
+      return dismiss
+    }
+
     return { currentPage }
   }
 })
