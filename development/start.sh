5222bfbe886eee171b32e95086f3859266fc617b
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-03-22 11:49:02 +0000

    Switch tar for RSync in development environment

--- development/start.sh
+++ development/start.sh
@@ -1,45 +1,16 @@
 #!/usr/bin/env
-
-expressjs_path=/app/expressjs/
-expressjs_tar=expressjs.tar
-
-ui_path=/app/ui/
-ui_tar=ui.tar
+destination=/app/
+source=dist/
 
 # Import env variables to container
 . .env_vars
 
-echo "Running in hot reload mode. Setting up the environment..."
+echo "Running in hot reload mode. Setting up the development environment..."
+start_time=$(date +%s)
 
-# Check if UI needs updating
-echo "Checking state of UI..."
-if tar --compare --file=$ui_tar --directory $ui_path &>/dev/null; then
-    echo "UI is up to date."
-else
-# Update volume with latest UI files
-    echo "Starting UI update..."
-    start_time=$(date +%s)
-    rm -rf $ui_path
-    mkdir -p $ui_path
-    tar xfp $ui_tar --directory $ui_path --atime-preserve
-    rm $ui_tar
-    echo "UI updated in $(($(date +%s)-$start_time)) seconds."
-fi
+rsync --archive --delete --inplace $source $destination
 
-# Check if ExpressJS needs updating
-echo "Checking state of ExpressJS..."
-if tar --compare --file=$expressjs_tar --directory $expressjs_path &>/dev/null; then
-    echo "ExpressJS is up to date."
-else
-# Update volume with latest ExpressJS files
-    echo "Starting ExpressJS update..."
-    start_time=$(date +%s)
-    rm -rf $expressjs_path
-    mkdir -p $expressjs_path
-    tar xfp $expressjs_tar --directory $expressjs_path --atime-preserve
-    rm $expressjs_tar
-    echo "ExpressJS updated in $(($(date +%s)-$start_time)) seconds."
-fi
+echo "Development environment ready. Setup took $(($(date +%s)-$start_time)) seconds."
 
-# Execute Supervisor
+# Start Supervisor
 exec supervisord -c supervisord.conf
