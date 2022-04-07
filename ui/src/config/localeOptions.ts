c22213de9aa0f68370e06c5eca728565c4412b72
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-04-07 16:45:54 +0100

    Seperate i18n menu items in to own file

---
+++ ui/src/config/localeOptions.ts
@@ -0,0 +1,17 @@
+// Langauges for the language switcher menu.
+
+// They will display in the same order as given here.
+
+// Value = name of the json file stored in the i18n file (without extension). Files must
+// follow the standard naming used by browsers to benefit from auto detection of locale.
+
+// Label = the name to display in the language switcher for that language.
+
+export default [
+  { value: 'en-US', label: 'English' },
+  { value: 'de', label: 'Deutsch' },
+  { value: 'fr', label: 'Français' },
+  { value: 'it', label: 'Italiano' },
+  { value: 'nb-NO', label: 'Norsk' },
+  { value: 'pt-BR', label: 'Português' }
+]
