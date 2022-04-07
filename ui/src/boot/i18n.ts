972f23927f92c7d9df7c0349d25f99c6b23f24c2
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-04-07 16:23:19 +0100

    Set languages earlier in the boot process

--- ui/src/boot/i18n.ts
+++ ui/src/boot/i18n.ts
@@ -1,6 +1,7 @@
+import enUS from 'src/i18n/en-US.json'
+import { Quasar, LocalStorage } from 'quasar'
 import { boot } from 'quasar/wrappers'
 import { createI18n, LocaleMessageDictionary, VueMessageType } from 'vue-i18n'
-import enUS from 'src/i18n/en-US.json'
 
 // Set default language
 const defaultLang = 'en-US'
@@ -12,31 +13,38 @@ const langGlob = import.meta.glob('src/i18n/*.json')
 // Create i18n instance
 const i18n = createI18n({
   globalInjection: true,
+  legacy: false,
   locale: 'en-US',
   messages: {
     'en-US': enUS
   }
 })
 
-export async function loadLanguageAsync(isoName: string) {
+// Set language to previously chosen according to local storage, otherwise use browser default
+if (LocalStorage.getItem('lang')) {
+  void loadLanguageAsync(LocalStorage.getItem('lang') as string)
+} else if (langGlob[`../i18n/${Quasar.lang.getLocale() as string}.json`]) {
+  void loadLanguageAsync(Quasar.lang.getLocale() as string)
+}
+
+export function loadLanguageAsync(isoName: string) {
+  // Store the chosen language in local storage
+  LocalStorage.set('lang', isoName)
+
   // If the language was already loaded
   if (loadedLanguages.includes(isoName)) {
-    return Promise.resolve(isoName)
+    return Promise.resolve((i18n.global.locale.value = isoName))
   }
 
   // If the language hasn't been loaded yet
-  if (langGlob[`../i18n/${isoName}.json`]) {
-    return await langGlob[`../i18n/${isoName}.json`]().then((messages) => {
-      i18n.global.setLocaleMessage(
-        isoName,
-        messages.default as LocaleMessageDictionary<VueMessageType>
-      )
-      loadedLanguages.push(isoName)
-      return Promise.resolve(isoName)
-    })
-  } else {
-    return Promise.resolve(defaultLang)
-  }
+  return langGlob[`../i18n/${isoName}.json`]().then((messages) => {
+    i18n.global.setLocaleMessage(
+      isoName,
+      messages.default as LocaleMessageDictionary<VueMessageType>
+    )
+    loadedLanguages.push(isoName)
+    return Promise.resolve((i18n.global.locale.value = isoName))
+  })
 }
 
 export default boot(({ app }) => {
