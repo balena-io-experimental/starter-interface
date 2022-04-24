6648ce395ba61e1782840ce5db347d98ebb70233
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-04-24 21:34:48 +0100

    Tweak various components

--- ui/src/config/localeOptions.ts
+++ ui/src/config/localeOptions.ts
@@ -1,8 +1,8 @@
-// Langauges for the language switcher menu.
+// Languages for the language switcher menu.
 
 // They will display in the same order as given here.
 
-// Value = name of the json file stored in the i18n file (without extension). Files must
+// Value = name of the json file stored in the i18n folder (without extension). Files must
 // follow the standard naming used by browsers to benefit from auto detection of locale.
 
 // Label = the name to display in the language switcher for that language.
