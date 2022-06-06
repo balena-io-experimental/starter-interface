90880dceee79e0eb7157aa7fb66d43e1ca611d8e
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-06-06 15:40:44 +0100

    Add SystemInfo case switcher

--- expressjs/src/routes/v1/System.ts
+++ expressjs/src/routes/v1/System.ts
@@ -1,11 +1,10 @@
 import * as dns from 'dns'
-import express, { Request, Response } from 'express'
+import express, { Request, RequestHandler, Response } from 'express'
 import si from 'systeminformation'
-import queueCache from '@/middleware/queueCache'
 import Logger from '@/common/logger'
 
 interface reqBodyData {
-  cmd: string
+  id: string
 }
 
 const router = express.Router()
@@ -25,32 +24,160 @@ router.get('/v1/system/internet_check', (_req, res) => {
 })
 
 // Fetch systeminfo
-router.post(
-  '/v1/system/systeminfo',
-  queueCache,
-  async (req: Request, res: Response) => {
-    try {
-      const reqBody = req.body as reqBodyData
+router.post('/v1/system/systeminfo', (async (req: Request, res: Response) => {
+  try {
+    let data = {}
+    const reqBody = req.body as reqBodyData
 
-      const data = (await getSiFunction(reqBody.cmd)) as JSON
-      res.json({ data })
-    } catch (error) {
-      Logger.error(error)
-      res.status(500)
-      res.json({ error })
+    switch (reqBody.id) {
+      case '/':
+        data = (await si.getAllData()) as []
+        break
+      case 'a':
+        data = await si.audio()
+        break
+      case 'B':
+        data = await si.baseboard()
+        break
+      case 'Y':
+        data = await si.battery()
+        break
+      case 'b':
+        data = await si.bios()
+        break
+      case 'e':
+        data = await si.blockDevices()
+        break
+      case 'h':
+        data = await si.bluetoothDevices()
+        break
+      case 'C':
+        data = await si.chassis()
+        break
+      case 'c':
+        data = await si.cpu()
+        break
+      case 'j':
+        data = await si.cpuCurrentSpeed()
+        break
+      case 'T':
+        data = await si.cpuTemperature()
+        break
+      case 'l':
+        data = await si.currentLoad()
+        break
+      case 'd':
+        data = await si.diskLayout()
+        break
+      case 'D':
+        data = await si.disksIO()
+        break
+      case '0':
+        data = await si.dockerContainerProcesses()
+        break
+      case '8':
+        data = await si.dockerContainers()
+        break
+      case '9':
+        data = await si.dockerContainerStats()
+        break
+      case '7':
+        data = await si.dockerImages()
+        break
+      case '6':
+        data = await si.dockerInfo()
+        break
+      case '+':
+        data = await si.dockerVolumes()
+        break
+      case 'E':
+        data = await si.fsOpenFiles()
+        break
+      case 'f':
+        data = await si.fsSize()
+        break
+      case 'F':
+        data = await si.fsStats()
+        break
+      case 'L':
+        data = await si.fullLoad()
+        break
+      case '.':
+        data = await si.getStaticData()
+        break
+      case 'g':
+        data = await si.graphics()
+        break
+      case 'm':
+        data = await si.mem()
+        break
+      case 'M':
+        data = await si.memLayout()
+        break
+      case '5':
+        data = await si.networkConnections()
+        break
+      case '2':
+        data = await si.networkGatewayDefault()
+        break
+      case '1':
+        data = await si.networkInterfaceDefault()
+        break
+      case '3':
+        data = await si.networkInterfaces()
+        break
+      case '4':
+        data = await si.networkStats()
+        break
+      case 'o':
+        data = await si.osInfo()
+        break
+      case 'r':
+        data = await si.printer()
+        break
+      case 'p':
+        data = await si.processes()
+        break
+      case 'S':
+        data = await si.shell()
+        break
+      case 'y':
+        data = await si.system()
+        break
+      case 'w':
+        data = await si.wifiNetworks()
+        break
+      case 'u':
+        data = await si.usb()
+        break
+      case 'z':
+        data = await si.users()
+        break
+      case 'U':
+        data = await si.uuid()
+        break
+      case 'V':
+        data = await si.vboxInfo()
+        break
+      case 'v':
+        data = await si.versions()
+        break
+      case 'x':
+        data = await si.wifiConnections()
+        break
+      case 'W':
+        data = await si.wifiInterfaces()
+        break
+      default:
+        throw new Error(`Method '${reqBody.id}' is not implemented.`)
     }
-  }
-)
 
-// Functions
-function getSiFunction(funcName: string) {
-  if (si[funcName as keyof typeof si]) {
-    // Disable warnings about using `any`
-    // eslint-disable-next-line
-    return (si as any)[funcName]()
+    res.json({ data })
+  } catch (error) {
+    Logger.error(error)
+    res.status(500)
+    res.json({ error })
   }
-
-  throw new Error(`Method '${funcName}' is not implemented.`)
-}
+}) as RequestHandler)
 
 export default router
