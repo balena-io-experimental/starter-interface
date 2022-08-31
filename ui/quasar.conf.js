b0bc892094b86f81cb864e34ccd423fdbbbd63c9
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-08-31 15:16:57 +0200

    Begin rename to starter interface

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -216,7 +216,7 @@ module.exports = configure(function (ctx) {
       builder: {
         // https://www.electron.build/configuration/configuration
 
-        appId: 'balena Device UI'
+        appId: 'balena Starter Interface'
       }
     },
 
