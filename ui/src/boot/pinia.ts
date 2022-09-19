49df90991db255bf44929b28a10d9e416c17a041
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-09-19 21:16:04 +0200

    Fix race condition on boot

--- ui/src/boot/pinia.ts
+++ ui/src/boot/pinia.ts
@@ -1,9 +1,11 @@
 // Pinia store actions to call on first boot
 import { networkSettings } from 'stores/system'
+import { boot } from 'quasar/wrappers'
 
-const networkStore = networkSettings()
-
-// Get Cloudlink status
-if (process.env.ON_DEVICE?.toLowerCase() === 'true') {
-  void networkStore.checkCloudlink()
-}
+export default boot(() => {
+  // Get Cloudlink status
+  if (process.env.ON_DEVICE?.toLowerCase() === 'true') {
+    const networkStore = networkSettings()
+    void networkStore.checkCloudlink()
+  }
+})
