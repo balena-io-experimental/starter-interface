3434b57edad0f388889eff76c9ce35e5dea4750f
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-10-14 21:09:51 +0200

    Fix TS lint error on Express 5

--- expressjs/src/routes/v1/Supervisor.ts
+++ expressjs/src/routes/v1/Supervisor.ts
@@ -6,7 +6,7 @@ import createAxiosInstance from '@/common/axios'
 import Logger from '@/common/logger'
 import queueCache from '@/middleware/queueCache'
 import request, { AxiosRequestConfig } from 'axios'
-import express, { Request, Response } from 'express'
+import express, { Request, RequestHandler, Response } from 'express'
 import path from 'path'
 import process from 'process'
 
@@ -32,53 +32,52 @@ supervisorAxios.defaults.headers.common.Authorization = `Bearer ${
 // -- Routes -- //
 // Note that this route uses the queueCache middleware set by `queueCache`.
 // Be aware of its implications when doing development.
-router.post(
-  '/v1/supervisor',
-  queueCache,
-  async (req: Request, res: Response) => {
-    const reqBody = req.body as BodyDataReq
+router.post('/v1/supervisor', queueCache, (async (
+  req: Request,
+  res: Response
+) => {
+  const reqBody = req.body as BodyDataReq
 
-    //  Unfortunately the Supervisor URLs are not currently standardised
-    // and this provides a workaround. If Balena App ID is required as
-    // part of the Supervisor URL then add it to the payload.
-    let url
-    if (reqBody.path2) {
-      url = path.join(
-        reqBody.path,
-        process.env.BALENA_APP_ID as string,
-        reqBody.path2
-      )
-    } else {
-      url = reqBody.path
-    }
-
-    // Construct the payload
-    const payload = {
-      data: reqBody.params,
-      method: reqBody.type,
-      url
-    }
-
-    // Send the request
-    try {
-      const response = await supervisorAxios(payload as AxiosRequestConfig)
-
-      // Return the same http code as the one Supervisor returned
-      res.status(response.status)
-      // Return the Supervisor response to the UI
-      res.json(response.data)
-      Logger.debug('Returned Supervisor data.')
-    } catch (error) {
-      // Mirror the Axios status code
-      if (request.isAxiosError(error) && error.response) {
-        res.status(error.response.status)
-      } else {
-        res.status(500)
-      }
-      // Return the error to the UI
-      res.json(error)
-    }
+  //  Unfortunately the Supervisor URLs are not currently standardised
+  // and this provides a workaround. If Balena App ID is required as
+  // part of the Supervisor URL then add it to the payload.
+  let url
+  if (reqBody.path2) {
+    url = path.join(
+      reqBody.path,
+      process.env.BALENA_APP_ID as string,
+      reqBody.path2
+    )
+  } else {
+    url = reqBody.path
   }
-)
+
+  // Construct the payload
+  const payload = {
+    data: reqBody.params,
+    method: reqBody.type,
+    url
+  }
+
+  // Send the request
+  try {
+    const response = await supervisorAxios(payload as AxiosRequestConfig)
+
+    // Return the same http code as the one Supervisor returned
+    res.status(response.status)
+    // Return the Supervisor response to the UI
+    res.json(response.data)
+    Logger.debug('Returned Supervisor data.')
+  } catch (error) {
+    // Mirror the Axios status code
+    if (request.isAxiosError(error) && error.response) {
+      res.status(error.response.status)
+    } else {
+      res.status(500)
+    }
+    // Return the error to the UI
+    res.json(error)
+  }
+}) as RequestHandler)
 
 export default router
