22537f724ae43d576bc115cfa69115cae471ca48
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2023-01-17 17:10:56 +0000

    Switch NUC test fleet for Generic fleet

--- expressjs/src/common/logger.ts
+++ expressjs/src/common/logger.ts
@@ -24,9 +24,8 @@ const level = () => {
   const env = process.env.NODE_ENV || 'development'
   const isDevelopment =
     env === 'development' ||
-    process.env.BALENA_APP_NAME === 'bdi-nuc' ||
-    process.env.BALENA_APP_NAME === 'bdi-rpi' ||
-    process.env.BALENA_APP_NAME === 'bdi-generic'
+    process.env.BALENA_APP_NAME === 'bdi-generic' ||
+    process.env.BALENA_APP_NAME === 'bdi-rpi'
   return isDevelopment ? 'debug' : 'warn'
 }
 
